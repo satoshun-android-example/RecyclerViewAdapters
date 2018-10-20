ebda3784e02f20cd8dd2ba39029dfbbc72cbd988
SatoShun <shun.sato1@gmail.com>
2018-10-20 04:16:14 +0000

    groupie secondn

--- groupie/src/main/java/com/github/satoshun/example/adapters/groupie/GroupieMainActivity.kt
+++ groupie/src/main/java/com/github/satoshun/example/adapters/groupie/GroupieMainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.satoshun.example.adapters.groupie.databinding.MainActBinding
+import com.github.satoshun.example.adapters.groupie.databinding.MainItemBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import com.xwray.groupie.databinding.BindableItem
 
 class GroupieMainActivity : AppCompatActivity() {
   private lateinit var binding: MainActBinding
@@ -14,5 +18,27 @@ class GroupieMainActivity : AppCompatActivity() {
 
     binding = DataBindingUtil.setContentView(this, R.layout.main_act)
     binding.recycler.layoutManager = LinearLayoutManager(this)
+    binding.recycler.adapter = MainAdapter()
   }
 }
+
+class MainAdapter : GroupAdapter<ViewHolder>() {
+  init {
+    add(MainItem(User(name = "tom")))
+    add(MainItem(User(name = "momo")))
+  }
+}
+
+class MainItem(
+  private val user: User
+) : BindableItem<MainItemBinding>() {
+  override fun getLayout(): Int = R.layout.main_item
+
+  override fun bind(binding: MainItemBinding, position: Int) {
+    binding.title.text = user.name
+  }
+}
+
+data class User(
+  val name: String
+)
