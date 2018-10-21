80042b78037faf6ecdef63f19848f35f8074caeb
SatoShun <shun.sato1@gmail.com>
2018-10-21 00:09:13 +0000

    second litho

---
+++ litho/src/main/java/com/github/satoshun/example/adapters/litho/App.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.example.adapters.litho
+
+import android.app.Application
+import com.facebook.soloader.SoLoader
+
+class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+  }
+}
--- litho/src/main/java/com/github/satoshun/example/adapters/litho/LithoMainActivity.kt
+++ litho/src/main/java/com/github/satoshun/example/adapters/litho/LithoMainActivity.kt
@@ -1,14 +1,46 @@
 package com.github.satoshun.example.adapters.litho
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.LithoView
+import com.facebook.litho.widget.LinearLayoutInfo
+import com.facebook.litho.widget.Recycler
+import com.facebook.litho.widget.RecyclerBinder
+import com.facebook.litho.widget.Text
 import com.github.satoshun.example.adapters.data.UserRepository
 import com.github.satoshun.example.adapters.epoxy.BaseActivity
+import kotlinx.coroutines.launch
 
 class LithoMainActivity : BaseActivity() {
   private val repository = UserRepository()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.main_act)
+    val componentContext = ComponentContext(this)
+
+    val recyclerBinder = RecyclerBinder
+        .Builder()
+        .layoutInfo(LinearLayoutInfo(LinearLayoutManager(this)))
+        .build(componentContext)
+    val component = Recycler.create(componentContext)
+        .binder(recyclerBinder)
+        .build()
+    val view = LithoView.create(this, component)
+    setContentView(view)
+
+    launch {
+      val users = repository.getUsers()
+      users.forEach {
+        recyclerBinder.appendItem(
+            Text.create(componentContext)
+                .text(it.name)
+                .textColor(Color.BLACK)
+                .textSizeDip(28f)
+                .build()
+        )
+      }
+    }
   }
 }
